f025aa71ec982b658ba399a005a590826c28b39f
DevDBreno <dbrenoti@gmail.com>
2020-10-30 08:39:15 -0300

    chore(interfaces): added CreatedResponse interface

--- src/interfaces/trainer.interface.ts
+++ src/interfaces/trainer.interface.ts
@@ -2,8 +2,9 @@ import { IsString, IsUUID, Length } from 'class-validator'
 
 import { TrainerEntity } from '@entities/trainer.entity'
 import { PokemonGateway } from '@interfaces/pokeapi.interface'
+import { HttpStatus } from '@nestjs/common'
 
-export type TrainerSanitized = Pick<TrainerEntity, 'pokemons' | 'save'>
+export type TrainerAddPokemon = Pick<TrainerEntity, 'pokemons' | 'save'>
 
 export class CreateTrainerDTO {
   @IsString()
@@ -29,6 +30,11 @@ export class AddPokemonParamsDTO {
 }
 
 export class AddPokemonDTO {
-  trainer: TrainerSanitized
+  trainer: TrainerAddPokemon
   pokemon: PokemonGateway
+}
+
+export interface CreatedResponse {
+  statusCode: HttpStatus.CREATED
+  message: string
 }
